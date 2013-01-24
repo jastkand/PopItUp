30c5634722c13d7cf093065ea60e73046908bbfd
Andrey Krivko <jastkand@gmail.com>
2013-01-25 02:55:09 +0400

    update style

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,5 @@
 (function($){
     $(function(){
-        var $content = document.getElementById("content");
-        $('body').css({
-            width: $content.offsetWidth + 20,
-            height: $content.offsetHeight + 100
-        });
+
     })
 })(jQuery);
